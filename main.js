9e5485ecbf2df000b628942d365d6c43a6dba652
Connie Park <chjpark14@gmail.com>
2016-06-19 18:11:34 -0500

    created initialize()

--- main.js
+++ main.js
@@ -4,13 +4,15 @@
 	//request data to be passed into the httpRequest function
 	var requestData = {
 		type: 'GET',
-		url: 'https://api.twitch.tv/kraken/search/streams?q=starcraft'
-	}
-	function httpRequest(type, url) {
+		url: 'https://api.twitch.tv/kraken/search/streams?q='
+	};
+	
+	function httpRequest(type, url, searchTerms) {
 		//promise for asynchronously loading of data
+
 		var requestPromise = new Promise(function(resolve, reject) {
 			var xmlHTTP = new XMLHttpRequest();
-			xmlHTTP.open(type, url, true);
+			xmlHTTP.open(type, url + searchTerms, true);
 			xmlHTTP.onload = function() {
 				if(xmlHTTP.readyState === 4 && xmlHTTP.status === 200) {
 					resolve(JSON.parse(xmlHTTP.responseText));
@@ -27,24 +29,32 @@
 		});
 
 		return requestPromise;
-	}
+	};
+
+	function appendResults(results) {
+
+	};
 
 	//initializes the app and sets up the events
-	window.addEventListener('load', function() {
-		var searchButton = document.getElementById('twitch-search-button');
+	function initialize() {
+		window.addEventListener('load', function() {
+			var searchButton = document.getElementById('twitch-search-button');
 
-		//set up the search Button's onclick
-		searchButton.onclick = function() {
+			//set up the search Button's onclick
+			searchButton.onclick = function() {
+				var searchTerm = document.getElementById('search-term').value;
 
-			//start the http request
-			httpRequest(requestData.type, requestData.url).then(function(response){
-				console.log(response);
-			}, function (err) {
-				console.log(err);
-			});
-		};
+				//start the http request
+				httpRequest(requestData.type, requestData.url, searchTerm).then(function(response){
+					console.log(response);
+				}, function (err) {
+					console.log(err);
+				});
+			};
 
-	}, false);
+		}, false);
+	};
 
+	initialize();
 })();
 //window.twitchStreamLoader;
